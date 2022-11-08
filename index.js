c6ed8fe23fb5ab30f06d25368300ab4a71fe1dac
asaad-tt <aasadshumon86@gmail.comm>
2022-11-08 19:18:56 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -11,6 +12,14 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("awesomely eating server running");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xz8clof.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
 app.listen(port, () => {
